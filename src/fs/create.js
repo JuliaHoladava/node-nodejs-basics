4b0e504c943b86cf48f23a1586c2c0e03746ea0d
Julia Holadava <juliaholadava@gmail.com>
2024-01-23 13:49:17 +0100

    feat: implement create.js function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,27 @@
+import fs from 'fs/promises';
+import path  from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const create = async () => {
-    // Write your code here 
+    const directoryPath = path.join(__dirname, 'files');
+    const filePath = path.join(directoryPath, 'fresh.txt');
+
+    try {
+        try {
+            await fs.access(filePath);
+            throw new Error('FS operation failed');
+        } catch (error) {
+            if (error.code !== 'ENOENT') throw error;
+        }
+
+        await fs.mkdir(directoryPath, { recursive: true });
+
+        await fs.writeFile(filePath, 'I am fresh and young');
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
 await create();
