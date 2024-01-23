1d11cacece4a2358b106387a2e1843ba3a2e6cdf
Julia Holadava <juliaholadava@gmail.com>
2024-01-23 22:14:28 +0100

    feat: implement delete.js function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const remove = async () => {
-    // Write your code here 
+    const removePath = path.join(__dirname, 'files', 'fileToRemove.txt');
+
+    try {
+        await fs.access(removePath);
+
+        await fs.rm(removePath);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed - file to remove does not exist');
+        } else {
+            console.error(error.message);
+        }
+    }
 };
 
 await remove();
