70591bd3a105a2cd07756bacde683889798f7f05
Julia Holadava <juliaholadava@gmail.com>
2024-01-24 10:52:16 +0100

    feat: implement read.js function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    try {
+        const data = await fs.readFile(filePath, 'utf8');
+        console.log(data);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            throw new Error('FS operation failed - file to read does not exist')
+        } else {
+            console.error(error.message);
+        }
+    }
 };
 
 await read();
