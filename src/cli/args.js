e8b1e751ec93987e95af4dde57e4d73a9432eae7
Julia Holadava <juliaholadava@gmail.com>
2024-01-25 10:58:02 +0100

    feat: implement args.js function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
+import { argv } from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+    const parsedArgs = {};
+
+    for (let i = 0; i < args.length; i += 2) {
+        const key = args[i].replace('--', '');
+        parsedArgs[key] = args[i + 1];
+    }
+
+    for (const [key, value] of Object.entries(parsedArgs)) {
+        console.log(`${key} is ${value}`);
+    }
 };
 
 parseArgs();
