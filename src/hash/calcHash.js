1da47ef2a1f21ea03385418411a904dd0f7baec9
Julia Holadava <juliaholadava@gmail.com>
2024-01-26 21:30:01 +0100

    feat: implement calcHash.js function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createReadStream } from 'fs';
+import { createHash } from 'crypto';
+import { dirname, join } from 'path';
+import { pipeline } from 'stream/promises';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+    const fileStream = createReadStream(filePath);
+    const hash = createHash('sha256');
+
+    try {
+        await pipeline(
+            fileStream,
+            hash
+        );
+        console.log(hash.digest('hex'));
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
 await calculateHash();
